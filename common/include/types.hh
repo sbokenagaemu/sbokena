d553d626bcbc1d8511d0bf0fd9bcbc07bc91ce01
flippette <me@flippo.cc>
2025-10-15 17:03:35 +0300

    common: add short aliases

---
+++ common/include/types.hh
@@ -0,0 +1,25 @@
+// short aliases for standard library types.
+
+#pragma once
+
+#include <cmath>
+#include <cstdint>
+
+namespace sbokena::utils {
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
+
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using usize = std::size_t;
+
+using f32 = std::float_t;
+using f64 = std::double_t;
+
+} // namespace sbokena::utils
