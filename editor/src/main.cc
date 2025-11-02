faaf5d20e8be78d2864aab4867aeeca661bcb363
horichd1 <david.horich@aalto.fi>
2025-11-02 17:55:13 +0000

    editor : added window

--- editor/src/main.cc
+++ editor/src/main.cc
@@ -1,10 +1,32 @@
 #ifndef RAYGUI_IMPLEMENTATION
 #define RAYGUI_IMPLEMENTATION
+#include "Window.hpp"
 #endif
 
+#include <nlohmann/json.hpp>
 #include <raygui.h>
 #include <raylib.h>
 
+// several resolution presets can be implemented via changing the values, while
+// retaining the ratio
+#define width 1800
+#define height 1000
+
 int main() {
-  return 0;
+  raylib::Window window(width, height, "Window");
+  window.SetTargetFPS(60);
+  bool exit = false;
+
+  while (!window.ShouldClose() && !exit) {
+    window.BeginDrawing();
+
+    // Clear background (testing purposes)
+    window.ClearBackground(GRAY);
+
+    // BUTTONS
+
+    // TILES
+
+    window.EndDrawing();
+  }
 }
