4a2ff832d8284b6d21ee9a8d5f2cb50dfae3a2b7
flippette <me@flippo.cc>
2025-11-09 22:03:04 +0200

    common: make everything in types.hh constexpr

--- common/include/types.hh
+++ common/include/types.hh
@@ -35,37 +35,66 @@ enum struct Direction : u8 {
 class Directions {
 public:
   // construct an empty directions set.
-  Directions() noexcept;
+  constexpr Directions() noexcept : flags_{0} {}
   // construct a directions set from a direction.
-  Directions(const Direction &) noexcept;
+  constexpr Directions(const Direction &dir) noexcept
+    : flags_{static_cast<u8>(dir)} {}
   // construct a directions set from a raw flag value.
   // this zeroes the upper bits of the value.
-  Directions(u8 flags) noexcept;
+  constexpr Directions(u8 flags) noexcept
+    : flags_{static_cast<u8>(flags & 0b1111)} {}
 
   // does this set contain some cardinal direction?
-  bool contains(const Direction &) const noexcept;
+  constexpr bool contains(const Direction &dir) const noexcept {
+    return flags_ & static_cast<u8>(dir);
+  }
 
   // does this set contain all of these directions?
-  bool contains_all(const Directions &) const noexcept;
+  constexpr bool contains_all(const Directions &dirs) const noexcept {
+    return !dirs.flags_ || (flags_ & dirs.flags_) == dirs.flags_;
+  }
 
   // does this set contain any of these directions?
-  bool contains_any(const Directions &) const noexcept;
+  constexpr bool contains_any(const Directions &dirs) const noexcept {
+    return !dirs.flags_ || (flags_ & dirs.flags_);
+  }
 
   // is this set empty?
-  bool empty() const noexcept;
+  constexpr bool empty() const noexcept {
+    return !flags_;
+  }
 
   // return the raw flag value.
-  u8 flags() const noexcept;
+  constexpr u8 flags() const noexcept {
+    return flags_;
+  }
 
 private:
   u8 flags_ = 0b0000;
 };
 
-Directions operator|(const Direction &, const Direction &) noexcept;
-Directions operator|(const Directions &, const Direction &) noexcept;
-Directions operator|(const Directions &, const Directions &) noexcept;
-Directions &operator|=(Directions &, const Direction &) noexcept;
-Directions &operator|=(Directions &, const Directions &) noexcept;
+constexpr Directions operator|(const Direction &lhs,
+                               const Direction &rhs) noexcept {
+  return {static_cast<u8>(static_cast<u8>(lhs) | static_cast<u8>(rhs))};
+}
+constexpr Directions operator|(const Directions &lhs,
+                               const Direction &rhs) noexcept {
+  return {static_cast<u8>(lhs.flags() | static_cast<u8>(rhs))};
+}
+constexpr Directions operator|(const Directions &lhs,
+                               const Directions &rhs) noexcept {
+  return {static_cast<u8>(lhs.flags() | rhs.flags())};
+}
+constexpr Directions &operator|=(Directions &lhs,
+                                 const Direction &rhs) noexcept {
+  lhs = lhs | rhs;
+  return lhs;
+}
+constexpr Directions &operator|=(Directions &lhs,
+                                 const Directions &rhs) noexcept {
+  lhs = lhs | rhs;
+  return lhs;
+}
 
 // a position in a 2D grid.
 template <std::integral T>
@@ -75,25 +104,43 @@ struct Position {
 
   // this position with x and y swapped.
   [[nodiscard("transposed() does not modify `this`")]]
-  Position transposed() const noexcept;
+  constexpr Position transposed() const noexcept {
+    return {y, x};
+  }
 };
 
 template <std::integral T>
-Position<T> &operator+=(Position<T> &, const Position<T> &) noexcept;
-
-template <std::integral T>
-Position<T> &operator-=(Position<T> &, const Position<T> &) noexcept;
-
-template <std::integral T>
 [[nodiscard("operator+() does not modify `this`")]]
-Position<T> operator+(const Position<T> &, const Position<T> &) noexcept;
+constexpr Position<T> operator+(const Position<T> &lhs,
+                                const Position<T> &rhs) noexcept {
+  return {lhs.x + rhs.x, lhs.y + rhs.y};
+}
 
 template <std::integral T>
 [[nodiscard("operator-() does not modify `this`")]]
-Position<T> operator-(const Position<T> &, const Position<T> &) noexcept;
+constexpr Position<T> operator-(const Position<T> &lhs,
+                                const Position<T> &rhs) noexcept {
+  return {lhs.x - rhs.x, lhs.y - rhs.y};
+}
 
 template <std::signed_integral T>
 [[nodiscard("operator-() does not modify `this`")]]
-Position<T> operator-(const Position<T> &) noexcept;
+constexpr Position<T> operator-(const Position<T> &pos) noexcept {
+  return {-pos.x, -pos.y};
+}
+
+template <std::integral T>
+constexpr Position<T> &operator+=(Position<T> &lhs,
+                                  const Position<T> &rhs) noexcept {
+  lhs = lhs + rhs;
+  return lhs;
+}
+
+template <std::integral T>
+constexpr Position<T> &operator-=(Position<T> &lhs,
+                                  const Position<T> &rhs) noexcept {
+  lhs = lhs - rhs;
+  return lhs;
+}
 
 } // namespace sbokena::types
