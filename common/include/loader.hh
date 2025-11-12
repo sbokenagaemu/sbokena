6dc2597c35c9b7abc029ec84a80c0270689f50cb
horichd1 <david.horich@aalto.fi>
2025-11-12 12:30:24 +0000

    loader: working loader

--- common/include/loader.hh
+++ common/include/loader.hh
@@ -2,38 +2,63 @@
 
 #pragma once
 
+#include <array>
+
+#include <Texture.hpp>
 #include <raylib.h>
-#include <vector>
 
-// indexed as the following) (for now)
-// 0  - roof (missing)
-// 1  - wall
-// 2  - floor
-// 3  - player
-// 4  - crate
-// 5  - button (missing)
-// 6  - door (open)
-// 7  - door (closed)
-// 8  - directional floor up
-// 9  - directional floor right
-// 10 - directional floor down
-// 11 - directional floor left
-// 12 - directional crate up
-// 13 - directional crate right
-// 14 - directional crate down
-// 15 - directional crate left
-// 16 - portal 1
-// 17 - portal 2
-// 18 - portal 3
+#include "types.hh"
 
-// themes_ vectors contain all textures required for a single theme (they are
-class TextureID {
+using namespace sbokena::types;
+
+class TileID {
 public:
-  TextureID();
-  std::vector<Image> GetTextures(int id);
+  operator usize() const {
+    return id_;
+  }
+
+  friend class Theme;
 
 private:
-  void load_textures();
-  int theme_count_;
-  std::vector<std::vector<Image>> themes_;
+  usize id_;
+
+  constexpr TileID(usize id) : id_{id} {}
 };
+
+class Theme {
+public:
+  Theme(std::string);
+
+  const std::string GetName() {
+    return name_;
+  }
+
+private:
+  static constexpr TileID ROOF = 0;
+  static constexpr TileID WALL = 1;
+  static constexpr TileID FLOOR = 2;
+  static constexpr TileID PLAYER = 3;
+  static constexpr TileID CRATE = 4;
+  static constexpr TileID BUTTON = 5;
+  static constexpr TileID DOOR_OPEN = 6;
+  static constexpr TileID DOOR_CLOSED = 7;
+  static constexpr TileID DIR_FLOOR_UP = 8;
+  static constexpr TileID DIR_FLOOR_RIGHT = 9;
+  static constexpr TileID DIR_FLOOR_DOWN = 10;
+  static constexpr TileID DIR_FLOOR_LEFT = 11;
+  static constexpr TileID DIR_CRATE_UP = 12;
+  static constexpr TileID DIR_CRATE_RIGHT = 13;
+  static constexpr TileID DIR_CRATE_DOWN = 14;
+  static constexpr TileID DIR_CRATE_LEFT = 15;
+  static constexpr TileID PORTAL = 16;
+
+  const std::string name_;
+
+  std::array<raylib::Texture, 17> textures_;
+
+  const raylib::Texture &tile(const TileID &id) const {
+    return textures_[id];
+  }
+};
+
+std::vector<std::string> valid_textures();
