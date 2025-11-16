f065ef5337a8771fa2d77268db3e5036f11bfb19
Tong Ki Chun <kichun.tong@aalto.fi>
2025-11-16 05:23:41 +0200

    Use conditional namespaces for size_t

--- common/include/types.hh
+++ common/include/types.hh
@@ -4,6 +4,7 @@
 
 #include <cmath>
 #include <cstdint>
+#include <cstddef>  // for size_t
 
 namespace sbokena::types {
 
@@ -17,7 +18,11 @@ using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
-using usize = std::size_t;
+#if defined(__APPLE__)
+using usize = size_t;  // macOS: global namespace
+#else
+using usize = std::size_t;  // Linux/other platforms
+#endif
 
 using f32 = std::float_t;
 using f64 = std::double_t;
