a36f115f29466c98f543cbd1fe9eb416e82931ef
Tong Ki Chun <kichun.tong@aalto.fi>
2025-11-16 04:54:01 +0200

    Add minimum main with window creation and rendering loop

--- game/src/main.cc
+++ game/src/main.cc
@@ -1,3 +1,22 @@
+#include <raylib.h>
+
 int main() {
-  return 0;
+#ifdef MACOS_COCOA_BACKEND
+    // macOS OpenGL 4.1 hints (GLFW handled internally by raylib)
+#endif
+
+    // Initialize window
+    InitWindow(800, 600, "Sbokena");
+    SetTargetFPS(60);
+
+    // Main loop
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("You are hacked!\n- Sbokena", 200, 200, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
