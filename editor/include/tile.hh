701732d7c815c8119aa93783a43ee8991e69fa41
hoanghuudao <huudao2536@gmail.com>
2025-11-24 17:13:25 +0200

    editor: give portal and onedir a dir in and dir out, door's state can be affected the object on it

--- editor/include/tile.hh
+++ editor/include/tile.hh
@@ -27,39 +27,39 @@ enum class TileType {
 // A class containing common fields and functions that all tiles share.
 class Tile {
 public:
-  // construct a new tile, contains no object by default
-  // obj_id = 0x00 -> no id linked (id indexing starts at 0x01)
+  // construct a new tile, contains no object by default.
+  // obj_id = 0x00 -> no id linked (id indexing starts at 0x01).
   explicit Tile(TileType type, u32 id) : type(type), id(id), obj_id(0x00) {}
 
-  // default destructor
+  // default destructor.
   virtual ~Tile() = default;
 
-  // returns tile type
+  // returns tile type.
   TileType get_type() const {
     return type;
   }
 
-  // tile's id
+  // tile's id.
   u32 get_id() const {
     return id;
   }
 
-  // if there is an object on the tile
+  // if there is an object on the tile.
   virtual bool contains_obj() const {
     return (obj_id != 0x00);
   }
 
-  // tile's object id
+  // tile's object id.
   virtual u32 get_obj_id() const {
     return obj_id;
   }
 
-  // object goes on tile
+  // object goes on tile.
   virtual void set_obj_id(const u32 &new_id) {
     obj_id = new_id;
   }
 
-  // object goes off tile
+  // object goes off tile.
   virtual void remove_obj_id() {
     obj_id = 0x00;
   }
@@ -74,70 +74,92 @@ private:
 // The default type for a tile. Does not and cannot contain any object.
 class Wall : public Tile {
 public:
-  // construct a new wall
+  // construct a new wall.
   explicit Wall(u32 id) : Tile(TileType::Wall, id) {}
 
-  // does not contain object
+  // does not contain object.
   bool contains_obj() const override {
     return false;
   }
 
-  // no object, no id
+  // no object, no id.
   u32 get_obj_id() const override {
     return 0x00;
   }
 
-  // object cannot be on wall
+  // object cannot be on wall.
   void set_obj_id(const u32 &) override {}
 
-  // does not have object to remove to begin with
+  // does not have object to remove to begin with.
   void remove_obj_id() override {}
 };
 
 // The basic, most common type of non-wall tile. Can contain an object.
 class Floor : public Tile {
 public:
-  // construct a new floor
+  // construct a new floor.
   Floor(u32 id) : Tile(TileType::Floor, id) {}
 };
 
-// Similar to floor but only lets object move through one specified way.
+// Similar to floor but only lets object move into it through one specified way.
+// To move out of the tile, the object can only move the opposite way.
 class OneDir : public Tile {
 public:
-  // construct a new one direction floor
-  // by default the direction is up
-  OneDir(u32 id) : Tile(TileType::OneDir, id), dir(Direction::Up) {}
+  // construct a new one direction floor.
+  // by default the direction into the tile is up.
+  OneDir(u32 id) : Tile(TileType::OneDir, id), dir_in(Direction::Up) {}
 
-  // to which only direction to move on this tile
-  Direction get_dir() const {
-    return dir;
+  // which only direction for the object to move into this tile.
+  Direction get_dir_in() const {
+    return dir_in;
   }
 
-  // can only move to this new direction on this tile
-  void set_dir(Direction newdir) {
-    dir = newdir;
-  }
-
-  // rotates clockwise
-  void rotate() {
-    switch (dir) {
+  // which only direction for the object to move out of this tile.
+  Direction get_dir_out() const {
+    Direction dir_out;
+    switch (dir_in) {
     case Direction::Up:
-      dir = Direction::Right;
+      dir_out = Direction::Down;
       break;
     case Direction::Right:
-      dir = Direction::Down;
+      dir_out = Direction::Left;
       break;
     case Direction::Down:
-      dir = Direction::Left;
+      dir_out = Direction::Up;
       break;
     case Direction::Left:
-      dir = Direction::Up;
+      dir_out = Direction::Right;
+      break;
+    }
+    return dir_out;
+  }
+
+  // can only move into and out of this tile by this new direction and its
+  // opposite direction respectively.
+  void set_dir_in(Direction newdir) {
+    dir_in = newdir;
+  }
+
+  // rotates clockwise.
+  void rotate() {
+    switch (dir_in) {
+    case Direction::Up:
+      dir_in = Direction::Right;
+      break;
+    case Direction::Right:
+      dir_in = Direction::Down;
+      break;
+    case Direction::Down:
+      dir_in = Direction::Left;
+      break;
+    case Direction::Left:
+      dir_in = Direction::Up;
       break;
     }
   }
 
 private:
-  Direction dir;
+  Direction dir_in;
 };
 
 // If the player arrives into this tile, the level is completed.
@@ -147,11 +169,14 @@ public:
 };
 
 // Can teleport objects to the linked portal.
+// Only lets object move in and out of the tile in a specific way and its
+// opposite way respectively.
 class Portal : public Tile {
 public:
-  // construct a new portal, by default by default is unlinked to another
-  // portal.
-  Portal(u32 id) : Tile(TileType::Portal, id), linked(false) {}
+  // construct a new portal, by default is unlinked to another portal.
+  // by default the direction into the tile is up.
+  Portal(u32 id)
+    : Tile(TileType::Portal, id), linked(false), dir_in(Direction::Up) {}
 
   // flags the portal as linked.
   void link() {
@@ -168,8 +193,58 @@ public:
     return linked;
   }
 
+  // which only direction for the object to move into this tile.
+  Direction get_dir_in() const {
+    return dir_in;
+  }
+
+  // which only direction for the object to move out of this tile.
+  Direction get_dir_out() const {
+    Direction dir_out;
+    switch (dir_in) {
+    case Direction::Up:
+      dir_out = Direction::Down;
+      break;
+    case Direction::Right:
+      dir_out = Direction::Left;
+      break;
+    case Direction::Down:
+      dir_out = Direction::Up;
+      break;
+    case Direction::Left:
+      dir_out = Direction::Right;
+      break;
+    }
+    return dir_out;
+  }
+
+  // can only move into and out of this tile by this new direction and its
+  // opposite direction respectively.
+  void set_dir_in(Direction newdir) {
+    dir_in = newdir;
+  }
+
+  // rotates clockwise.
+  void rotate() {
+    switch (dir_in) {
+    case Direction::Up:
+      dir_in = Direction::Right;
+      break;
+    case Direction::Right:
+      dir_in = Direction::Down;
+      break;
+    case Direction::Down:
+      dir_in = Direction::Left;
+      break;
+    case Direction::Left:
+      dir_in = Direction::Up;
+      break;
+    }
+  }
+
 private:
   bool linked;
+  Direction dir_in;
 };
 
 // Opens when the linked button is pressed.
@@ -195,13 +270,21 @@ public:
   }
 
   // opens the door.
-  void open() {
+  // if there is an object on the door, can't change the door's state.
+  bool open() {
+    if (contains_obj())
+      return false;
     opened = true;
+    return true;
   }
 
   // closes the door.
-  void close() {
+  // if there is an object on the door, can't change the door's state.
+  bool close() {
+    if (contains_obj())
+      return false;
     opened = false;
+    return true;
   }
 
   // is the door opened or closed.
