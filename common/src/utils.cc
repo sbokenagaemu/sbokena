e323d69a1d8444bda4633f8f1fb09f3d82cc23a7
htphuong28 <thieuphuong5knt@gmail.com>
2025-11-26 17:16:10 +0200

    common: add file UI helpers

---
+++ common/src/utils.cc
@@ -0,0 +1,58 @@
+#include "utils.hh"
+
+#include <filesystem>
+#include <optional>
+#include <span>
+
+#include <nfd.h>
+#include <nfd.hpp>
+
+namespace fs = std::filesystem;
+
+namespace sbokena::utils {
+
+static constexpr nfdwindowhandle_t NULL_HANDLE = {
+  .type = NFD_WINDOW_HANDLE_TYPE_UNSET,
+  .handle = nullptr,
+};
+
+std::optional<fs::path>
+open_file_dialog(std::span<const nfdfilteritem_t> filter) {
+  NFD::UniquePath path;
+
+  const auto res =
+    NFD::OpenDialog(path, filter.data(), filter.size(), nullptr, NULL_HANDLE);
+
+  if (res != NFD_OKAY)
+    return std::nullopt;
+
+  fs::path path_ = path.get();
+  return std::optional<fs::path>(path_);
+}
+
+std::optional<fs::path> open_folder_dialog() {
+  NFD::UniquePath path;
+
+  const auto res = NFD::PickFolder(path);
+
+  if (res != NFD_OKAY)
+    return std::nullopt;
+
+  fs::path path_ = path.get();
+  return std::optional<fs::path>(path_);
+}
+
+std::optional<fs::path>
+save_file_dialog(std::span<const nfdfilteritem_t> filter) {
+  NFD::UniquePath path;
+
+  const auto res = NFD::SaveDialog(path, filter.data(), filter.size(), NULL,
+                                   "untitiled.sb", NULL_HANDLE);
+  if (res != NFD_OKAY)
+    return std::nullopt;
+
+  fs::path path_ = path.get();
+  return std::optional<fs::path>(path_);
+}
+
+} // namespace sbokena::utils
