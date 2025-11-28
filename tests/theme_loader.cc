5403faeb31530b6243e4b809662bba1450659cfa
flippette <me@flippo.cc>
2025-11-28 04:05:25 +0200

    common: theme loader test draft

---
+++ tests/theme_loader.cc
@@ -0,0 +1,26 @@
+// `loader::Theme` loading and validation test.
+
+#include <filesystem>
+
+#include <gtest/gtest.h>
+
+#include "loader.hh"
+#include "utils.hh"
+
+namespace fs = std::filesystem;
+
+using sbokena::utils::Deferred;
+
+namespace sbokena::loader {
+
+// try to find and load the `dev` theme (inside `themes/dev`)
+// this should work when called both from the repository root,
+// as well as when called by `ctest` in `build/tests`.
+TEST(common, theme_load_ok) {
+  const auto cwd   = fs::current_path();
+  const auto theme = Theme::load("dev", cwd);
+  ASSERT_TRUE(theme.has_value());
+  // destroy it, see if unloading fails
+}
+
+} // namespace sbokena::loader
