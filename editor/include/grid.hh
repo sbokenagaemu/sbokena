513bddd3ed4fb674866c9730aa7159338cb99ec4
horichd1 <david.horich@aalto.fi>
2025-11-30 20:55:28 +0000

    editor: add missing function comments

--- editor/include/grid.hh
+++ editor/include/grid.hh
@@ -4,15 +4,19 @@
 
 #include <Window.hpp>
 
+// checks whether a position is in the the specified area
 bool is_inside(
   raylib::Vector2 pos, raylib::Vector2 min, raylib::Vector2 max
 );
 
+// returns the the new grid offset based on the resized grid
 raylib::Vector2 refactored_grid_offset(
   raylib::Vector2 offset_pos, raylib::Vector2 point_pos, float scale
 );
 
+// returns the max position in the grid
 raylib::Vector2 grid_end(raylib::Vector2 offset_pos, float tile_size);
 
+// returns the index of the tile that has the specific position
 raylib::Vector2
 tile_index(raylib::Vector2 pos, raylib::Vector2 offset, float size);
