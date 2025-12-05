923a94d92282fd67b0e957df2b3b9fa56fb2c8e3
htphuong28 <thieuphuong5knt@gmail.com>
2025-12-05 06:05:13 +0200

    game: test pushing box normally

--- tests/game_state_step.cc
+++ tests/game_state_step.cc
@@ -76,4 +76,42 @@ TEST(game, state_walk_dirfloor) {
   std::get<Player>(st.objects.at({.x = 3, .y = 1}));
 }
 
+TEST(game, state_push_normal) {
+  // █████
+  // █p☐ █
+  // █████
+  State st = {
+    .completed_goals = 0,
+    .tiles =
+      {{{.x = 1, .y = 1}, {Floor {}}},
+       {{.x = 2, .y = 1}, {Floor {}}},
+       {{.x = 3, .y = 1}, {Floor {}}}},
+    .objects =
+      {{{.x = 1, .y = 1}, {Player {}}}, {{.x = 2, .y = 1}, {Box {}}}},
+    .doors   = {},
+    .portals = {},
+  };
+
+  // █████
+  // █ p☐█
+  // █████
+  // step right, push box normally
+  ASSERT_EQ(st.step(Direction::Right), StepResult::Ok);
+  std::get<Player>(st.objects.at({.x = 2, .y = 1}));
+  std::get<Box>(st.objects.at({.x = 3, .y = 1}));
+
+  // step right, push box on wall
+  ASSERT_EQ(st.step(Direction::Right), StepResult::StepOnWall);
+  std::get<Player>(st.objects.at({.x = 2, .y = 1}));
+  std::get<Box>(st.objects.at({.x = 3, .y = 1}));
+
+  // █████
+  // █p ☐█
+  // █████
+  // step left, detach from box
+  ASSERT_EQ(st.step(Direction::Left), StepResult::Ok);
+  std::get<Player>(st.objects.at({.x = 1, .y = 1}));
+  std::get<Box>(st.objects.at({.x = 3, .y = 1}));
+}
+
 } // namespace sbokena::game::state
