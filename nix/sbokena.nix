f13e43f8087fe1e3b9c8774d2ae9b002a0a3ba9d
flippette <me@flippo.cc>
2025-12-18 20:50:30 +0100

    use nixpkgs' fetchers instead of builtins

--- nix/sbokena.nix
+++ nix/sbokena.nix
@@ -5,6 +5,7 @@
   enableX11 ? true,
   # nixpkgs
   lib,
+  fetchgit,
   clangStdenv,
   llvmPackages,
   xorg,
@@ -25,28 +26,28 @@
 }: let
   # sources of vendored external libraries
   vendoredSources = {
-    googletest-src = builtins.fetchGit {
+    googletest-src = fetchgit {
       url = "https://github.com/google/googletest";
       ref = "v1.17.0";
       rev = "52eb8108c5bdec04579160ae17225d66034bd723";
     };
-    nfd-src = builtins.fetchGit {
+    nfd-src = fetchgit {
       url = "https://github.com/btzy/nativefiledialog-extended";
       rev = "86d5f2005fe1c00747348a12070fec493ea2407e";
     };
-    nlohmann-json-src = builtins.fetchGit {
+    nlohmann-json-src = fetchgit {
       url = "https://github.com/nlohmann/json";
       rev = "55f93686c01528224f448c19128836e7df245f72";
     };
-    raylib-src = builtins.fetchGit {
+    raylib-src = fetchgit {
       url = "https://github.com/raysan5/raylib";
       rev = "c1ab645ca298a2801097931d1079b10ff7eb9df8";
     };
-    raylib-cpp-src = builtins.fetchGit {
+    raylib-cpp-src = fetchgit {
       url = "https://github.com/RobLoach/raylib-cpp";
       rev = "6d9d02cd242e8a4f78b6f971afcf992fb7a417dd";
     };
-    raygui-src = builtins.fetchGit {
+    raygui-src = fetchgit {
       url = "https://github.com/raysan5/raygui";
       rev = "25c8c65a6e5f0f4d4b564a0343861898c6f2778b";
     };
